f88d428e25e04b0e966df50f65f87d6974fa6305
Secret1337 <97087168+secret-pommes@users.noreply.github.com>
2024-12-03 12:06:32 +0100

    init

---
+++ Sharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Extensions.Hosting;
+using Sharp.Routes;
+
+var builder = WebApplication.CreateBuilder(args);   
+var app = builder.Build();
+
+app.Mapdatarouter();
+
+
+app.MapGet("/{*url}", (HttpContext context) =>
+{
+    Console.WriteLine($"Unmatched route accessed: {context.Request.Path}");
+
+    context.Response.StatusCode = 404;
+    context.Response.Headers.Add("Error", "404");
+
+    return "Error 404: Not Found";
+});
+
+app.Run();
---
+++ Sharp/Routes/datarouter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharp.Routes
+{
+    internal static class Rdatarouter
+    {
+        public static void Mapdatarouter (this WebApplication web)
+        {
+            var app = web.MapGroup("/datarouter");
+
+            app.MapGet("/api/v1/public/data", (HttpContext c) =>
+            {
+                c.Response.StatusCode = 204;
+                return "";
+            });
+        }
+    }
+}
